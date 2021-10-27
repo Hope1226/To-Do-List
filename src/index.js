3d5347217d58a7201c9e37b081ee9aa1807884c4
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-27 12:00:30 +0500

    Modify js and css file as per linters

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import MoreIcon from './more-icon.svg';
 import './style.css';
 
 const taskCont = document.querySelector('.taskContainer');
 
 const taskList = [
   {
-    description:'Wash the dishes',
+    description: 'Wash the dishes',
     completed: false,
-    index: 0
+    index: 0,
   },
   {
     description: 'Complete to Do list project',
     completed: false,
-    index: 0
-  }
+    index: 0,
+  },
 ];
 
 for (let i = 0; i < taskList.length; i += 1){
@@ -21,5 +20,5 @@ for (let i = 0; i < taskList.length; i += 1){
     <div class="task">
       <span class="checkbox"></span>
       <p class="taskTitle">${taskList[i].description}</p>
-    </div>`;
+    </div>`
 };
