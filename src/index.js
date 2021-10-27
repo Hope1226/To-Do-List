75d9afffdfba7ecb4020bb243d8b4132232d22bc
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-27 23:12:25 +0500

    Add update Local Str module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './style.css';
-import {updateComplation} from './updateStatus';
+import { updateComplation } from './updateStatus';
+import { updateLocalStr } from './updateLocalStorage';
 
 
 const taskCont = document.querySelector('.taskContainer');
-
+const localData = localStorage.getItem('localTasks');
 
 const taskList = [
   {
@@ -18,18 +19,40 @@ const taskList = [
   },
 ];
 
+if(localData){
+  let localList = JSON.parse(localData);
+  for (let i = 0; i < localList.length; i += 1) {
+    if(localList[i].completed){
+    taskCont.innerHTML += `
+      <div class="task checked">
+      <input type="checkbox" class="checkbox" name="completion" id="${i}" value="completion" checked>
+        <p class="taskTitle">${localList[i].description}</p>
+      </div>`;
+    } else {
+      taskCont.innerHTML += `
+      <div class="task">
+      <input type="checkbox" class="checkbox" name="completion" id="${i}" value="completion">
+        <p class="taskTitle">${localList[i].description}</p>
+      </div>`;
+    };
+  };
+}
+else {
 for (let i = 0; i < taskList.length; i += 1) {
   taskCont.innerHTML += `
     <div class="task">
     <input type="checkbox" class="checkbox" name="completion" id="${i}" value="completion">
       <p class="taskTitle">${taskList[i].description}</p>
     </div>`;
+  };
 };
 
 const checkboxes = document.querySelectorAll(".checkbox");
 
 checkboxes.forEach(checkbox => {
   checkbox.addEventListener('change', (e)=>{
-    updateComplation(taskList, checkbox, e.target.id)
+    updateComplation(taskList, checkbox, e.target.id);
+    updateLocalStr(taskList);
   });
  });
+ 
