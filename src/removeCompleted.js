e69c91a202af79a63caf9f581396047abac5725f
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-29 14:35:26 +0500

    Moify module as per ESlint

--- src/removeCompleted.js
+++ src/removeCompleted.js
@@ -1,6 +1,6 @@
 const removeCompleted = (list, updateDisplay, updateLcSrg, updateStatus, cont) => {
-  const nonComplete = list.filter((task)=>{
-    if (!task.completed) return true;
+  const nonComplete = list.filter((task) => {
+    if (!task.completed) return task;
   });
 
   for (let i = 0; i < nonComplete.length; i += 1) {
@@ -13,8 +13,8 @@ const removeCompleted = (list, updateDisplay, updateLcSrg, updateStatus, cont) =
 
   checkboxes.forEach((checkbox) => {
     checkbox.addEventListener('change', (e) => {
-    updateStatus(nonComplete, checkbox, e.target.id);
-    updateLcSrg(nonComplete);
+      updateStatus(nonComplete, checkbox, e.target.id);
+      updateLcSrg(nonComplete);
     });
   });
 };
