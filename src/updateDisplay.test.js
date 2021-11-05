6a4ea05b8dc42559fa14783f0f48328384d7752d
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-11-05 19:40:36 +0500

    Add test for updateDis function

---
+++ src/updateDisplay.test.js
@@ -0,0 +1,15 @@
+import mainPage from "./__mock__/dom";
+import updateDis from "./updateDisplay";
+import { lint } from "stylelint";
+
+test("Update display", () => {
+  const page = mainPage.window.document.body;
+  const container = page.querySelector('.taskContainer');
+  const list = [{
+    index: 1,
+    description: "some text",
+  }];
+  updateDis(list, container);
+  const tasks = container.querySelectorAll('.task');
+  expect(tasks).toHaveLength(1);
+}); 
