417e23054c6a58abfaaaaa7427ac6f834271dc5d
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-11-06 06:24:04 +0500

    Add tests for the rest functions

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,9 @@
 import updateLocalStr from './updateLocalStorage.js';
-import mainPage from './__mock__/dom.js';
 import removeCompleted from './__mock__/removeCompleted.js';
+import mainPage from './__mock__/dom.js';
 import addTasks from './addTask.js';
+import updateComplation from './updateStatus.js';
+import updateDis from './updateDisplay.js';
 
 describe('Pure functions', () => {
   test('Add taskt', () => {
@@ -39,5 +41,44 @@ describe('Pure functions', () => {
     updateLocalStr(output);
     const updatedStorage = JSON.parse(localStorage.getItem('localTasks'));
     expect(updatedStorage).toHaveLength(1);
+  });
+  test('Update display', () => {
+    const dom = mainPage.window.document.body;
+    dom.innerHTML = `
+    <div class="item taskContainer"></div>`;
+    const container = dom.querySelector('.taskContainer');
+    console.log(container)
+    const list = [{
+      index: 1,
+      description: 'some text',
+    }];
+    updateDis(list, container);
+    const tasks = dom.querySelectorAll('.task')
+    expect(tasks).toHaveLength(1);
+  });
+  test('Should  return true if the the storage is the same as list', () => {
+    const list = [{
+      key: 'value',
+    }];
+    updateLocalStr(list);
+    const storage = JSON.parse(localStorage.getItem('localTasks'));
+    expect(storage).toHaveLength(1);
+  });
+  test('Update Complation', () => {
+    const dom = mainPage.window.document.body;
+    dom.innerHTML = `
+    <div class="task">
+        <input type="checkbox" class="checkbox" name="completion" id="0" value="completion" checked>
+          <p class="taskTitle">Some text</p>
+    </div>`;
+    const checkbox = dom.querySelector('.checkbox');
+    const list = [
+      {
+        index: 1,
+        completed: false,
+      },
+    ];
+    updateComplation(list, checkbox, 0);
+    expect(list[0].completed).toBeTruthy();
   });
 });
